9a23e3dde08de084439fa3789373f2bfa6a5ad1a
Florian Fankhauser <flo@lingu.at>
2023-03-16 16:30:25 +0100

    add media keys

--- config.h
+++ config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -60,6 +62,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "x-terminal-emulator", NULL };
 
+static const char *mutecmd[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *switchsink[] = { "switch-sink.sh", NULL };
+static const char *brupcmd[] = { "brightnessctl", "set", "+2%", NULL };
+static const char *brdowncmd[] = { "brightnessctl", "set", "2%-",  NULL };
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -94,7 +103,13 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} },
+	{ 0,                            XF86XK_AudioMute,                        spawn,          {.v = mutecmd } },
+	{ 0,                            XF86XK_AudioLowerVolume,                 spawn,          {.v = voldowncmd } },
+	{ 0,                            XF86XK_AudioRaiseVolume,                 spawn,          {.v = volupcmd } },
+	{ 0,                            XF86XK_MonBrightnessUp,                  spawn,          {.v = brupcmd} },
+	{ 0,                            XF86XK_MonBrightnessDown,                spawn,          {.v = brdowncmd} },
+	{ MODKEY|ShiftMask,             XK_p,                                    spawn,          {.v = switchsink} },
 };
 
 /* button definitions */
